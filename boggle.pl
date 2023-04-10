fadf15c7ba3099950848a67c09daac875d993ed9
Hasansiddi <48926261+Hasansiddi@users.noreply.github.com>
2023-04-10 16:08:04 -0700

    Add files via upload

--- boggle.pl
+++ boggle.pl
@@ -5,6 +5,17 @@
 % dynamically define the list of characters
 characters(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']).
 
+% Facts about adjacent indexes
+adjacent_indexes(0, [1,3,4]).
+adjacent_indexes(1, [0,3, 4,5,2]).
+adjacent_indexes(2, [1,4,5]).
+adjacent_indexes(3, [0,1,4,7,6]).
+adjacent_indexes(4, [0,1,2,3,5,7,6,8]).
+adjacent_indexes(5, [2,1,4,7,8]).
+adjacent_indexes(6, [3,4,7]).
+adjacent_indexes(7, [6,3,4,5,8]).
+adjacent_indexes(8, [5,4,7]).
+
 % generate N by N boggle board
 generate_boggle_board(N, M) :-
     length(M, N),
@@ -71,59 +82,42 @@ solve_boggle_board_helper(Board, Index, UsedIndexes, Word, Result) :-
 adjacent_positions(Index, AdjacentIndexes) :-
     adjacent_indexes(Index, AdjacentIndexes).
 
-adjacent_indexes(0, [1,3,4]).
-adjacent_indexes(1, [0,3, 4,5,2]).
-adjacent_indexes(2, [1,4,5]).
-adjacent_indexes(3, [0,1,4,7,6]).
-adjacent_indexes(4, [0,1,2,3,5,7,6,8]).
-adjacent_indexes(5, [2,1,4,7,8]).
-adjacent_indexes(6, [3,4,7]).
-adjacent_indexes(7, [6,3,4,5,8]).
-adjacent_indexes(8, [5,4,7]).
+
 
 dictionary(A) :-
     string_to_atom(A, X),
     s(_,_,X,_,_,_).
 
+% run('z','e','b','r','a','l','t','s','e').
 run(A, B, C, D, E, F, G, H, I) :-
     new(@p, picture('Boggle Board')),
     send(@p, open),
+
+    % First row
     send(@p, display, new(@box1, box(50,50))),
     send(@p, display, new(@text1, text(A)), point(10,10)),
     send(@p, display, new(@box2, box(100,50))),
     send(@p, display, new(@text2, text(B)), point(60,10)),
     send(@p, display, new(@box3, box(150,50))),
     send(@p, display, new(@text3, text(C)), point(110,10)),
-    % send(@p, display, new(@box4, box(200,50))),
-    % send(@p, display, new(@text4, text('D')), point(160,10)),
 
+    % Second row
     send(@p, display, new(@box5, box(50,100))),
     send(@p, display, new(@text5, text(D)), point(10,50)),
     send(@p, display, new(@box6, box(100,100))),
     send(@p, display, new(@text6, text(E)), point(60,50)),
     send(@p, display, new(@box7, box(150,100))),
     send(@p, display, new(@text7, text(F)), point(110,50)),
-    % send(@p, display, new(@box8, box(200,100))),
-    % send(@p, display, new(@text8, text('H')), point(160,50)),
 
+    % Third row
     send(@p, display, new(@box9, box(50,150))),
     send(@p, display, new(@text9, text(G)), point(10,100)),
     send(@p, display, new(@box10, box(100,150))),
     send(@p, display, new(@text10, text(H)), point(60,100)),
     send(@p, display, new(@box11, box(150,150))),
     send(@p, display, new(@text11, text(I)), point(110,100)),
-    % send(@p, display, new(@box12, box(200,150))),
-    % send(@p, display, new(@text12, text('L')), point(160,100)),
 
-    % send(@p, display, new(@box13, box(50,200))),
-    % send(@p, display, new(@text13, text('M')), point(10,150)),
-    % send(@p, display, new(@box14, box(100,200))),
-    % send(@p, display, new(@text14, text('N')), point(60,150)),
-    % send(@p, display, new(@box15, box(150,200))),
-    % send(@p, display, new(@text15, text('O')), point(110,150)),
-    % send(@p, display, new(@box16, box(200,200))),
-    % send(@p, display, new(@text16, text('P')), point(160,150)),
-
+    % Display the answers
     send(@p, display, new(@answer1, text('The possible words are: ')), point(100,150)),
     solve_boggle_board([A,B,C,D,E,F,G,H,I], WordsWithDuplicate),
     sort(WordsWithDuplicate, Words),
@@ -131,6 +125,7 @@ run(A, B, C, D, E, F, G, H, I) :-
     flatten_and_concat(Result, FinalText),
     send(@p, display, new(@answer2, text(FinalText)), point(100,170)).
 
+% Turns [['f', 'o', 'o'], ['b', 'a', 'r']] into ['foo', 'bar']
 concat_all([], Result, Result).
 concat_all([H|T], RSF, Result) :-
     atomic_list_concat(H, '', Atm),
@@ -138,17 +133,7 @@ concat_all([H|T], RSF, Result) :-
     string_concat(Str, " ", Str_with_space),
     concat_all(T, [Str_with_space|RSF], Result).
 
-% concat_all([], []).
-% concat_all([H|T], Result) :-
-%     atomic_list_concat(H, '', Atm),
-%     atom_string(Atm, Str),
-%     string_concat(Str, " ", Str_with_space),
-%     concat_all(T, Str_with_space|Result).
-
+% Turns ['foo', 'bar'] into "foo bar"
 flatten_and_concat(A, Result) :-
     flatten(A, L),
     atomic_list_concat(L, ",", Result).
-
-% board to use to test for words ['z','e','b','r','a','l','t','s','e'] 
-% concat_all([['z','e','a','l'],['a','t']], X).
-% run('z','e','b','r','a','l','t','s','e').
